1b7e57f3d93fd64d563a3426bd4483a6c8a461cd
Evil-Ezio <kekansuraj8@gmail.com>
2022-11-18 14:03:01 +0530

    workcard updated

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -3,14 +3,14 @@ import proimg3 from "../assests/pro3.jpg";
 const ProjectCardData = [
     {
         imgsrc : proimg3,
-        title : "Ecommerce Website Design",
+        title : "Movie Recommendation System",
         text: "kjyyrgfbcjhdsgvbcyyrgfībvkssgfu",
         view : "url.com",
         source : "url.com",
     },
     {
         imgsrc : proimg3,
-        title : "Ecommerce Website Design",
+        title : "Mega project",
         text: "kjyyrgfbcjhdsgvbcyyrgfībvkssgfu",
         view : "url.com",
         source : "url.com",
